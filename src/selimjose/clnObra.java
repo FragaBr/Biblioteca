d29f5b145283043064502c3abe8a9bb5fb65ad74
Bruna <Bruna@192.168.1.6>
2016-09-09 22:59:50 -0300

    parei em obra

--- src/selimjose/clnObra.java
+++ src/selimjose/clnObra.java
@@ -22,5 +22,52 @@ public class clnObra {
     protected int CdEditora;
     protected int CdAutor;
     
-     
+    public int getCdObra (){
+        return CdObra;
+    }
+    public void setCdObra(int CdObra){
+        this.CdObra = CdObra;
+    }
+    public String getTitulo(){
+        return Titulo;
+    }
+    public void setTitulo(String Titulo){
+        this.Titulo = Titulo;
+    }
+    public int getEdicao(){
+        return Edicao;
+    }
+    public void setEdicao(int Edicao){
+        this.Edicao = Edicao;
+    }
+    public int getAno(){
+        return Ano;
+    }
+    public void setAno(int Ano){
+        this.Ano = Ano;
+    }
+    public String getVolume(){
+        return Volume;
+    }
+    public void setVolume(String Volume){
+        this.Volume = Volume;
+    }
+    public String getISBN(){
+        return ISBN;
+    }
+    public void setISBN(String ISBN){
+        this.ISBN = ISBN;
+    }
+    public int getCdEditora(){
+        return CdEditora;
+    }
+    public void setCdEditora(int CdEditora){
+        this.CdEditora = CdEditora;
+    }
+    public int getCdAutor(){
+        return CdAutor;
+    }
+     public void setCdAutor(int CdAutor){
+        this.CdAutor = CdAutor;
+    }
 }
